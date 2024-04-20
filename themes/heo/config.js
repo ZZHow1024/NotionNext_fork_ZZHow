f02b4938336edc3c1cd28a4ccd83a927fb50a2e5
ZZHow <109335896+zzhow0@users.noreply.github.com>
2024-04-20 22:20:47 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -23,7 +23,7 @@ const CONFIG = {
   HEO_HERO_TITLE_LINK: 'https://blog.zzhow.com',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: 'Java', url: 'https://blog.zzhow.com/tag/Java' },
+  HEO_HERO_CATEGORY_1: { title: '学技术', url: 'https://blog.zzhow.com/tag/%E5%AD%A6%E6%8A%80%E6%9C%AF' },
   HEO_HERO_CATEGORY_2: { title: '期末复习', url: 'https://blog.zzhow.com/tag/%E6%9C%9F%E6%9C%AB%E5%A4%8D%E4%B9%A0' },
   HEO_HERO_CATEGORY_3: { title: 'CTF', url: 'https://blog.zzhow.com/tag/CTF' },
 
